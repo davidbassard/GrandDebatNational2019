ee12d930bb6acda917a66aaf1bccf6bd03318c39
Glitch6996 <davidbassard@gmail.com>
2019-11-29 20:36:48 +0100

    Initial commit

---
+++ dashboard.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------------------------------------------------------------------------------------------
+# Author: David Bassard
+# Date: 15/11/2019
+# ------------------------------------------------------------------------------------------------------------------
+
+# Libraries
+
+from app import app
+
+# ------------------------------------------------------------------------------------------------------------------
+# In the terminal window, in the folder "GrandDebatNational2019", execute the application with the following command:
+
+# $set FLASK_APP=dashboard.py
+# $set FLASK_DEBUG=1
+# $python -m flask run
+
+# And Ctrl + click to follow link: http://127.0.0.1:5000/
+
